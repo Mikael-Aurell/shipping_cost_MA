d935bd7fc712d993270b3f0306b99266decbeab3
Mikael Aurell <aurell.mikael@gmail.com>
2021-04-12 20:47:55 +0200

    Calculate Cost working fine

--- src/main/java/se/lexicon/shipping_cost/controller/ShippingController.java
+++ src/main/java/se/lexicon/shipping_cost/controller/ShippingController.java
@@ -3,15 +3,19 @@ package se.lexicon.shipping_cost.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import se.lexicon.shipping_cost.entity.Box;
 import se.lexicon.shipping_cost.repository.BoxRepository;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 
 @Controller
@@ -44,7 +48,13 @@ public class ShippingController {
     }
 
     @PostMapping("/add")
-    public String add(@ModelAttribute("box") Box box) {
+    public String add(/*@Valid*/ @ModelAttribute("box") Box box /*BindingResult bindingResult*/) {
+
+        /*if(bindingResult.hasErrors()){
+            return "addBoxForm";
+        }*/
+
+        box.setCost(calculateCost(box));
         boxRepository.save(box);
         System.out.println("box = " + box);
         return "redirect:/shipping/list";
@@ -56,5 +66,26 @@ public class ShippingController {
         iterable.iterator().forEachRemaining(boxList::add);
 
         return boxList;
+    }
+
+    Double calculateCost(Box box){
+        double setCost=0.0;
+        if (box != null) {
+            if (box.getCountry().equals("SW")) {
+                if (box.getWeightType().equals("g")) {
+                    setCost = ((box.getWeight() * 2.0) * 2.5);
+                } else {
+                    setCost = ((box.getWeight() * 1000.0) * 2.5);
+                }
+            }
+            if (box.getCountry().equals("AU")) {
+                if (box.getWeightType().equals("g")) {
+                    setCost = ((box.getWeight() * 2.0) * 7.0);
+                } else {
+                    setCost = ((box.getWeight() * 1000.0) * 7.0);
+                }
+            }
+        }
+        return setCost;
     }
 }
